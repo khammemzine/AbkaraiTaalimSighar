8292ad526a562563713a9922632a1a9d937de830
yacine arkane eddine <khammemzine@gmail.com>
2025-08-05 23:54:04 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: '127.0.0.1', // بدل "::1"
-    port: 3000         // أو أي منفذ متاح
+    host: '127.0.0.1', // استخدام IPv4 بدل ::1
+    port: 3000,         // أو أي منفذ آخر (مثل 5174)
+    strictPort: true    // لمنع تغيير المنفذ تلقائيًا
   }
-}
+})
